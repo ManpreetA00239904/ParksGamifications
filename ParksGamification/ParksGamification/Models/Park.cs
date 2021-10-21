ec1702fbc2e6877c0f107302b1bf0cbe7b3b1e7a
ManpreetA00239904 <manpreetsid90@gmail.com>
2021-10-21 20:57:52 +0530

    commit 2

---
+++ ParksGamification/ParksGamification/Models/Park.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParksGamification.Models
+{
+    class Park
+    {
+    }
+}
---
+++ ParksGamification/ParksGamification/Services/ParkDataStore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParksGamification.Services
+{
+    class ParkDataStore
+    {
+    }
+}
