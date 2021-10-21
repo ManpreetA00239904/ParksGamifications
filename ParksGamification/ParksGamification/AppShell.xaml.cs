7fde83ea886672ce38b998031d71caa405211cd3
ManpreetA00239904 <manpreetsid90@gmail.com>
2021-10-21 20:40:13 +0530

    Commit 1

---
+++ ParksGamification/ParksGamification/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ParksGamification
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
